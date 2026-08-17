b5bdb7271d5718431e37e2a814b6d821f89fa761
gabrielsenatore <gabriel.senatore2@gmail.com>
2026-08-17 16:19:58 +0800

    Add files via upload

---
+++ sw-2.js
@@ -0,0 +1,21 @@
+const CACHE = 'pai-v2';
+const ASSETS = ['./', './index.html', './icon.png', './manifest.json'];
+self.addEventListener('install', e => {
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).catch(() => {}));
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim()));
+});
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  const u = e.request.url;
+  if (u.indexOf('anthropic') > -1 || u.indexOf('claude') > -1) return;
+  e.respondWith(
+    caches.match(e.request).then(hit => hit || fetch(e.request).then(res => {
+      const copy = res.clone();
+      caches.open(CACHE).then(c => c.put(e.request, copy)).catch(() => {});
+      return res;
+    }).catch(() => caches.match('./index.html')))
+  );
+});
